51ac93c87386f88b422c7a08e3c109637961c8b0
Haarissams <haarissams02@gmail.com>
2024-09-01 13:12:51 +0530

    commit changes

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-		const conn = await mongoose.connect(process.env.MONGO_DB);
+		const conn = await mongoose.connect("mongodb+srv://Ecommerce:3DmoO4umPEHqGauh@cluster0.imtf4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 		console.log(`MongoDB connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.log("Error connecting to MONGODB", error.message);
